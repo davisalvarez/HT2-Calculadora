83c3af87741967db7c1cecb1110ac10427194145
DavisAlvarez <DavisAlvarez@192.168.1.12>
2016-07-23 14:31:04 -0600

    Separacion de expresion en un vector String[]

--- HT2-Calculadora/src/org/uvg/calculadora/Calculadora.java
+++ HT2-Calculadora/src/org/uvg/calculadora/Calculadora.java
@@ -3,23 +3,32 @@ package org.uvg.calculadora;
 import java.io.FileReader;
 import java.io.File;
 import java.io.BufferedReader;
+import java.util.ArrayList;
 public class Calculadora {
+	
+	private ArrayList<String[]> expresiones;
+	
+	public Calculadora(){
+		expresiones = new ArrayList<String[]>();
+	}
 	
 	public void lectorArchivo(){
 		File datos;
 		FileReader datosLeidos=null;
 		BufferedReader br;
 		try{
-			datos = new File ("datos.txt");
+			datos = new File ("./datos.txt");
 			datosLeidos = new FileReader (datos);
 			br = new BufferedReader(datosLeidos);
 			
-			String expresion = br.readLine(); 
-			
-			System.out.println("El archivo:");
-			System.out.println(expresion);
+			//String expresion = br.readLine(); 
+			String linea;
+			while((linea=br.readLine())!=null){
+            		String [] expresionDividida = linea.split(" ");
+	            	expresiones.add(expresionDividida);
+			}
 		}catch(Exception e){
-			//e.printStackTrace();
+			e.printStackTrace();
 		}
 		
 		try{                    
@@ -27,7 +36,7 @@ public class Calculadora {
             	datosLeidos.close();     
             }                  
          }catch (Exception e){ 
-           // e.printStackTrace();
+            e.printStackTrace();
          }
 	}
 	
